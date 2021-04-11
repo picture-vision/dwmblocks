ffe630cf507970d82f95d6467715bcdb5222f895
BaTemaN <fw@bateman.de>
2021-04-11 13:47:58 +0200

    changed block order

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ",	"pamixer --get-volume",																	 															 0,		10},
-	{" ",	"sensors | awk '/^Package/ { print $4 }' | sed s/+//g",																 5,		 0},
-	{" ",	"iwconfig wlp2s0 | awk '/ESSID/ {split($4,a,\":\"); print a[2]}' | sed s/\\\"//g",		10,		 0},
-	{" ", 	"acpi | awk '/^Battery/ { print $4}' | sed s/,//g", 																	30, 	 0},
-	{" ", 	"xbacklight | awk '{split($0,a,\".\"); print a[1]}'", 		 														 0, 	20},
-	{" ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",														10,		 0},
-	{" ", 	"date '+%b %d (%a) %I:%M%p'",																													 5,		 0},
+	{"  ", 	"acpi | awk '/^Battery/ { print $4}' | sed s/,//g", 																30, 	 0},
+	{" ",	"sensors | awk '/^Package/ { print $4 }' | sed s/+//g",																 5,		 0},
+	{" ",	"pamixer --get-volume-human",														 															 0,		10},
+	{" ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 													10,		 0},
+	{" ", 	"xbacklight | awk '{split($0,a,\".\"); {  print a[1] \"%\"}}'", 		 														 0, 	20},
+	{" ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",														10,		 0},
+	{" ", 	"date '+%b %d (%a) %I:%M%p' ",																												 5,		 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
