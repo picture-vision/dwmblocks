8fa8cf83c353b2c0a3d3e0ad33a351b62405ee4c
BaTemaN <fw@bateman.de>
2021-05-13 10:46:16 +0200

    switched to firacode nerd font

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ",	"sensors | awk '/^Package/ { print $4 }' | sed s/+//g",				 5,		 0},
+	{"  ",	"sensors | awk '/^Package/ { print $4 }' | sed s/+//g",				 5,		 0},
 	{" ", 	"acpi | awk '/^Battery/ { print $4}' | sed s/,//g", 				30, 	 0},
 	{" ",	"pamixer --get-volume-human",										 0,		10},
-	{" ", 	"xbacklight | awk '{split($0,a,\".\"); {  print a[1] \"%\"}}'", 	 0, 	20},
+	{"ﯦ ", 	"xbacklight | awk '{split($0,a,\".\"); {  print a[1] \"%\"}}'", 	 0, 	20},
 	{" ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",			10,		 0},
 	{" ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 	10,		 0},
 	{" ", 	"date '+%b %d (%a) %I:%M%p' ",										 5,		 0},
