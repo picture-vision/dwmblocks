7bc8679ee3fe84a87cc6927433cb5a6ecd04aac2
FW <fw@loki.valhalla.lan>
2022-08-20 08:46:00 +0200

    switched to brighnessctl

--- blocks.def.h
+++ blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"  ",	"sensors | awk '/^Package/ { print $4 }' | sed s/+//g",             5,		 0},
 	{"  ", 	"acpi | awk '/^Battery/ { print $4}' | sed s/,//g",             30, 	 0},
 	{" ",	"pamixer --get-volume-human",                                       0,		10},
-	{"ﯦ ", 	"xbacklight | awk '{split($0,a,\".\"); {  print a[1] \"%\"}}'",     0, 	    20},
+	{"ﯦ ", 	"echo $(echo $(brightnessctl g)00/$(brightnessctl m) | bc)%",     0, 	    20},
 	{"  ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",			10,		 0},
 //	{" ",	"nmcli c | awk '/wifi/ {print $1}'",		 	                    10,		 0},
 	{" ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 	10,		 0},
