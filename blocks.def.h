2001a704e74c95d3dd7738b22f05ca504c577a7a
FW <fw@picture-vision.com>
2023-01-22 19:16:01 +0100

    updated dwmblocks

--- blocks.def.h
+++ blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"^b#3b4252^^c#d65d0e^  ",	"sensors | awk '/^SMBUSMASTER/ { print $3 }' | sed s/+//g",             5,                    0   },
 	{"^b#3b4252^^c#98971a^ ",	"echo $(pamixer --get-volume-human | awk '{ print $1}')",           0,                    10  },
 	{"^b#3b4252^^c#d79921^  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",			    10,		                0   },
-	{"^b#3b4252^^c#b16286^  ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 	      30,		                0   },
+	// {"^b#3b4252^^c#b16286^  ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 	      30,		                0   },
 	{"^b#3b4252^^c#427b58^  ", "date '+%d.%b %H:%M ' ",									                          30,		                0   },
 };
 
