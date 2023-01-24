97dd92e09a6d061acf7afc69dfa06985fe1ec7f2
FW <fw@picture-vision.com>
2023-01-25 00:21:57 +0100

    updated blocks

--- blocks.def.h
+++ blocks.def.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	                  /*Command*/		                                                      /*Update Interval*/   /*Update Signal*/
 	{"^b#3b4252^^c#d65d0e^  ",	"sensors | awk '/^SMBUSMASTER/ { print $3 }' | sed s/+//g",             5,                    0   },
+	{"^b#3b4252^^c#b16286^ ",	"sensors | awk '/^edge/ { print $2 }' | sed s/+//g",             5,                    0   },
 	{"^b#3b4252^^c#98971a^ ",	"echo $(pamixer --get-volume-human | awk '{ print $1}')",           0,                    10  },
 	{"^b#3b4252^^c#d79921^  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",			    10,		                0   },
 	// {"^b#3b4252^^c#b16286^  ",	"iwctl station wlan0 show | awk '/network/ {print $3}'",		 	      30,		                0   },
