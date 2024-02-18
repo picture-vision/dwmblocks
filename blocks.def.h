e63012739679a812d14f5731e28b750714f09172
FW <fw@picture-vision.com>
2024-02-18 11:07:44 +0100

    updated sensors

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	                  /*Command*/		                                                        /*Update Interval*/   /*Update Signal*/
 	{"\x07 󰻠 \x06",	"sensors | awk '/^Tctl/ { print $2 }' | sed s/+//g",                  5,                    0   },
-	{"\x07 󰢮 \x06",	"sensors | awk '/^edge/ { print $2 }' | sed s/+//g",                  5,                    0   },
+	//{"\x07 󰢮 \x06",	"sensors | awk '/^edge/ { print $2 }' | sed s/+//g",                  5,                    0   },
 	{"\x07  \x06",	"echo $(pamixer --get-volume-human | awk '{ print $1}')",             0,                    10  },
 	{"\x07 󰍛 \x06", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",			      10,		                0   },
 	{"\x07 󰃭 \x06", "date +%c",									                                          30,		                0   },
