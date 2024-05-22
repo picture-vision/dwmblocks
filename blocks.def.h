fb576ce5d077661c85b800c7db1fea38aa4c593f
FW <fw@picture-vision.com>
2024-05-22 05:35:46 +0200

    minor code formatting

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/  /*Command*/								/*Update Interval*/	/*Update Signal*/
 	{" ",	  "echo $(playerctl metadata title)",					                            10,			11   },
 	{" 󰦝 ",	  "/usr/local/bin/vpnstatus",					                                   2,			 0   },
-	{"  ",	  "echo $(pamixer --get-volume-human | awk '{ print $1}')",              0,			10   },
+	{"   ",	  "echo $(pamixer --get-volume-human | awk '{ print $1}')",              0,			10   },
 	{" 󰻠 ",	  "sensors | awk '/^Tctl/ { print $2 }' | sed s/+//g",                   5,			 0   },
 	// {" 󰍛 ",   "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		10,			0   },
 	{" 󰃭 ",   "date '+%d.%b %H:%M '  ",						                                  30,			 0   },
